66a22c38717af6569e1c1abacdf109f07ed99338
Craigr99 <craigred99@gmail.com>
2022-02-15 10:21:53 +0000

    Added cards to home screen

--- screens/Home.js
+++ screens/Home.js
@@ -9,14 +9,37 @@ import {
   Link,
   Text,
   View,
+  Progress,
+  HStack,
+  Spacer,
 } from "native-base";
+import { useState } from "react";
 import { SafeAreaView } from "react-native";
 import { useSelector } from "react-redux";
+import { FontAwesome } from "@expo/vector-icons";
 
-const Home = ({ navigation }) => {
+const Home = ({ navigation, route }) => {
+  const testarray = [1, 2, 3];
+  // State
   const user = useSelector((state) => state.user.value);
   const nordigenToken = useSelector((state) => state.auth.nordigenToken);
   const authToken = useSelector((state) => state.auth.authToken);
+  const [accountBalance, setAccountBalance] = useState("");
+
+  // get account balance
+  // useState(() => {
+  //   axios
+  //     .get(
+  //       `https://ob.nordigen.com/api/v2/accounts/${route.params.accountID}/balances/`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${nordigenToken.nordigenToken}`,
+  //         },
+  //       }
+  //     )
+  //     .then((res) => setAccountBalance(res.data.balances[0]))
+  //     .catch((err) => console.log(err));
+  // });
 
   const logoutUser = () => {
     axios
@@ -55,8 +78,8 @@ const Home = ({ navigation }) => {
               <Text fontSize="lg" color="amber.400">
                 Welcome back,
               </Text>
-              <Text fontSize="3xl" color="white" fontWeight="bold">
-                {user.name}
+              <Text fontSize="3xl" color="white" fontWeight={600}>
+                {user.name || "John Doe"}
               </Text>
             </Flex>
 
@@ -68,9 +91,90 @@ const Home = ({ navigation }) => {
             />
           </Flex>
         </Box>
-        <Button size="lg" mb="4" bg="blue.500" py="3" onPress={logoutUser}>
+        {/* <Button size="lg" mb="4" bg="blue.500" py="3" onPress={logoutUser}>
           Logout
-        </Button>
+        </Button> */}
+        {accountBalance ? (
+          <Text color="white">
+            Balance available : €{accountBalance.balanceAmount.amount}
+          </Text>
+        ) : (
+          <Text></Text>
+        )}
+        {/* Instant Cash Card */}
+        <Box
+          mx="5"
+          mt="12"
+          px="6"
+          py="8"
+          bg="white"
+          borderRadius="xl"
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Flex>
+            <Text fontSize="lg" fontWeight={600}>
+              Instant
+            </Text>
+            <Text color="coolGray.500">Cash Available</Text>
+          </Flex>
+          <Text fontWeight={700} fontSize="xl">
+            €429.00
+          </Text>
+        </Box>
+
+        {/* Savings Card */}
+        <Box
+          mx="5"
+          mt="8"
+          px="6"
+          py="8"
+          bg="white"
+          borderRadius="xl"
+          shadow="2"
+        >
+          <Flex
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Box>
+              <Text fontSize="lg" fontWeight={600}>
+                Savings
+              </Text>
+              <Text color="coolGray.500">In your savings</Text>
+            </Box>
+            <Text fontWeight={700} fontSize="xl">
+              €1,292.00
+            </Text>
+          </Flex>
+          <Flex mt="4" direction="row" flexWrap="wrap" justify="space-between">
+            {testarray.map((item, index) => (
+              <Box
+                key={index}
+                borderColor="coolGray.200"
+                borderWidth={1}
+                p="3"
+                w="48%"
+                mb={4}
+              >
+                <FontAwesome name="plane" size={30} color="#4584FF" />
+                <Text fontWeight={600} fontSize="md" my="3">
+                  €250.00/
+                  <Text fontWeight={200} fontSize="sm" color="coolGray.500">
+                    €350.00
+                  </Text>
+                </Text>
+                <Progress colorScheme="emerald" value={55} mb="5" />
+              </Box>
+            ))}
+            {/* Add button */}
+            <Box alignSelf="center" alignItems="center" w="48%">
+              <Text>btn</Text>
+            </Box>
+          </Flex>
+        </Box>
       </SafeAreaView>
     </View>
   );
