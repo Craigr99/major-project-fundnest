b6602f5bcbe0a770ffb02b65ea7ef09e11210c19
Craigr99 <craigred99@gmail.com>
2022-03-23 23:21:20 +0000

    changed user image

--- screens/user/Index.js
+++ screens/user/Index.js
@@ -137,11 +137,11 @@ const Index = ({ navigation, route }) => {
                 bg="purple.600"
                 alignSelf="center"
                 size="xl"
-                source={{
-                  uri: "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-                }}
+                // source={{
+                //   uri: "https://images.unsplash.com/photo-1519052537078-e6302a4968d4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+                // }}
               >
-                RB
+                JD
               </Avatar>
             </Pressable>
             <Flex ml="6" direction="column">
